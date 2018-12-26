f1ed1015ac57e021fc607106e877220a606aae42
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-12-26 15:38:01 +0700

    Create UI

--- VBDota2WinPredic/VBDota2WinPredic/Form1.Designer.vb
+++ VBDota2WinPredic/VBDota2WinPredic/Form1.Designer.vb
@@ -23,43 +23,360 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.HeroSelect1 = New System.Windows.Forms.ComboBox()
+        Me.PictureHero1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.HeroSelect2 = New System.Windows.Forms.ComboBox()
+        Me.PictureHero2 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.HeroSelect3 = New System.Windows.Forms.ComboBox()
+        Me.PictureHero3 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.HeroSelect4 = New System.Windows.Forms.ComboBox()
+        Me.PictureHero4 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.HeroSelect5 = New System.Windows.Forms.ComboBox()
+        Me.PictureHero5 = New System.Windows.Forms.PictureBox()
+        Me.LableResult = New System.Windows.Forms.Label()
+        Me.GetResult = New System.Windows.Forms.Button()
+        Me.TabControlMain = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.InformationLabel = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.GraphLabel = New System.Windows.Forms.Label()
+        Me.GraphSelecter = New System.Windows.Forms.ComboBox()
+        Me.GraphDisplay = New System.Windows.Forms.PictureBox()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.ButtonReset = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureHero1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureHero2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.PictureHero3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox4.SuspendLayout()
+        CType(Me.PictureHero4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox5.SuspendLayout()
+        CType(Me.PictureHero5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControlMain.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.GraphDisplay, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage3.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.PictureBox1)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Controls.Add(Me.HeroSelect1)
+        Me.GroupBox1.Controls.Add(Me.PictureHero1)
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 107)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(758, 149)
+        Me.GroupBox1.Size = New System.Drawing.Size(135, 184)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.GroupBox1.Text = "Hero1"
         '
-        'PictureBox1
+        'HeroSelect1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(6, 19)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(71, 75)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
+        Me.HeroSelect1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.HeroSelect1.FormattingEnabled = True
+        Me.HeroSelect1.Location = New System.Drawing.Point(7, 158)
+        Me.HeroSelect1.MaxDropDownItems = 35
+        Me.HeroSelect1.Name = "HeroSelect1"
+        Me.HeroSelect1.Size = New System.Drawing.Size(121, 21)
+        Me.HeroSelect1.TabIndex = 1
+        '
+        'PictureHero1
+        '
+        Me.PictureHero1.Location = New System.Drawing.Point(6, 19)
+        Me.PictureHero1.Name = "PictureHero1"
+        Me.PictureHero1.Size = New System.Drawing.Size(121, 133)
+        Me.PictureHero1.TabIndex = 0
+        Me.PictureHero1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.HeroSelect2)
+        Me.GroupBox2.Controls.Add(Me.PictureHero2)
+        Me.GroupBox2.Location = New System.Drawing.Point(147, 107)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(135, 184)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Hero2"
+        '
+        'HeroSelect2
+        '
+        Me.HeroSelect2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.HeroSelect2.FormattingEnabled = True
+        Me.HeroSelect2.Location = New System.Drawing.Point(6, 157)
+        Me.HeroSelect2.MaxDropDownItems = 35
+        Me.HeroSelect2.Name = "HeroSelect2"
+        Me.HeroSelect2.Size = New System.Drawing.Size(121, 21)
+        Me.HeroSelect2.TabIndex = 2
+        '
+        'PictureHero2
+        '
+        Me.PictureHero2.Location = New System.Drawing.Point(6, 19)
+        Me.PictureHero2.Name = "PictureHero2"
+        Me.PictureHero2.Size = New System.Drawing.Size(121, 133)
+        Me.PictureHero2.TabIndex = 0
+        Me.PictureHero2.TabStop = False
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.HeroSelect3)
+        Me.GroupBox3.Controls.Add(Me.PictureHero3)
+        Me.GroupBox3.Location = New System.Drawing.Point(288, 107)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(135, 184)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Hero3"
+        '
+        'HeroSelect3
+        '
+        Me.HeroSelect3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.HeroSelect3.FormattingEnabled = True
+        Me.HeroSelect3.Location = New System.Drawing.Point(6, 158)
+        Me.HeroSelect3.MaxDropDownItems = 35
+        Me.HeroSelect3.Name = "HeroSelect3"
+        Me.HeroSelect3.Size = New System.Drawing.Size(121, 21)
+        Me.HeroSelect3.TabIndex = 3
+        '
+        'PictureHero3
+        '
+        Me.PictureHero3.Location = New System.Drawing.Point(6, 19)
+        Me.PictureHero3.Name = "PictureHero3"
+        Me.PictureHero3.Size = New System.Drawing.Size(121, 133)
+        Me.PictureHero3.TabIndex = 0
+        Me.PictureHero3.TabStop = False
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.HeroSelect4)
+        Me.GroupBox4.Controls.Add(Me.PictureHero4)
+        Me.GroupBox4.Location = New System.Drawing.Point(429, 107)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(135, 184)
+        Me.GroupBox4.TabIndex = 2
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Hero4"
+        '
+        'HeroSelect4
+        '
+        Me.HeroSelect4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.HeroSelect4.FormattingEnabled = True
+        Me.HeroSelect4.Location = New System.Drawing.Point(6, 157)
+        Me.HeroSelect4.MaxDropDownItems = 35
+        Me.HeroSelect4.Name = "HeroSelect4"
+        Me.HeroSelect4.Size = New System.Drawing.Size(121, 21)
+        Me.HeroSelect4.TabIndex = 4
+        '
+        'PictureHero4
+        '
+        Me.PictureHero4.Location = New System.Drawing.Point(6, 19)
+        Me.PictureHero4.Name = "PictureHero4"
+        Me.PictureHero4.Size = New System.Drawing.Size(121, 133)
+        Me.PictureHero4.TabIndex = 0
+        Me.PictureHero4.TabStop = False
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.HeroSelect5)
+        Me.GroupBox5.Controls.Add(Me.PictureHero5)
+        Me.GroupBox5.Location = New System.Drawing.Point(570, 107)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(135, 184)
+        Me.GroupBox5.TabIndex = 2
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Hero5"
+        '
+        'HeroSelect5
+        '
+        Me.HeroSelect5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.HeroSelect5.FormattingEnabled = True
+        Me.HeroSelect5.Location = New System.Drawing.Point(6, 157)
+        Me.HeroSelect5.MaxDropDownItems = 35
+        Me.HeroSelect5.Name = "HeroSelect5"
+        Me.HeroSelect5.Size = New System.Drawing.Size(121, 21)
+        Me.HeroSelect5.TabIndex = 5
+        '
+        'PictureHero5
+        '
+        Me.PictureHero5.Location = New System.Drawing.Point(6, 19)
+        Me.PictureHero5.Name = "PictureHero5"
+        Me.PictureHero5.Size = New System.Drawing.Size(121, 133)
+        Me.PictureHero5.TabIndex = 0
+        Me.PictureHero5.TabStop = False
+        '
+        'LableResult
+        '
+        Me.LableResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LableResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.LableResult.Location = New System.Drawing.Point(288, 297)
+        Me.LableResult.Name = "LableResult"
+        Me.LableResult.Size = New System.Drawing.Size(414, 58)
+        Me.LableResult.TabIndex = 3
+        Me.LableResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GetResult
+        '
+        Me.GetResult.Location = New System.Drawing.Point(6, 297)
+        Me.GetResult.Name = "GetResult"
+        Me.GetResult.Size = New System.Drawing.Size(135, 58)
+        Me.GetResult.TabIndex = 4
+        Me.GetResult.Text = "คำนวนผลแพ้ชนะ"
+        Me.GetResult.UseVisualStyleBackColor = True
+        '
+        'TabControlMain
+        '
+        Me.TabControlMain.Controls.Add(Me.TabPage1)
+        Me.TabControlMain.Controls.Add(Me.TabPage2)
+        Me.TabControlMain.Controls.Add(Me.TabPage3)
+        Me.TabControlMain.Location = New System.Drawing.Point(11, 10)
+        Me.TabControlMain.Name = "TabControlMain"
+        Me.TabControlMain.SelectedIndex = 0
+        Me.TabControlMain.Size = New System.Drawing.Size(720, 511)
+        Me.TabControlMain.TabIndex = 5
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.InformationLabel)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(712, 485)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Information"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'InformationLabel
+        '
+        Me.InformationLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.InformationLabel.Location = New System.Drawing.Point(27, 26)
+        Me.InformationLabel.Name = "InformationLabel"
+        Me.InformationLabel.Size = New System.Drawing.Size(643, 436)
+        Me.InformationLabel.TabIndex = 0
+        Me.InformationLabel.Text = "....."
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.GraphLabel)
+        Me.TabPage2.Controls.Add(Me.GraphSelecter)
+        Me.TabPage2.Controls.Add(Me.GraphDisplay)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(712, 485)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "กราฟสถิติต่างๆ"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'GraphLabel
+        '
+        Me.GraphLabel.AutoSize = True
+        Me.GraphLabel.Location = New System.Drawing.Point(185, 24)
+        Me.GraphLabel.Name = "GraphLabel"
+        Me.GraphLabel.Size = New System.Drawing.Size(76, 13)
+        Me.GraphLabel.TabIndex = 2
+        Me.GraphLabel.Text = "กราฟสถิติต่างๆ"
+        '
+        'GraphSelecter
+        '
+        Me.GraphSelecter.FormattingEnabled = True
+        Me.GraphSelecter.Location = New System.Drawing.Point(286, 21)
+        Me.GraphSelecter.Name = "GraphSelecter"
+        Me.GraphSelecter.Size = New System.Drawing.Size(234, 21)
+        Me.GraphSelecter.TabIndex = 1
+        '
+        'GraphDisplay
+        '
+        Me.GraphDisplay.Location = New System.Drawing.Point(6, 48)
+        Me.GraphDisplay.Name = "GraphDisplay"
+        Me.GraphDisplay.Size = New System.Drawing.Size(700, 376)
+        Me.GraphDisplay.TabIndex = 0
+        Me.GraphDisplay.TabStop = False
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.ButtonReset)
+        Me.TabPage3.Controls.Add(Me.GroupBox1)
+        Me.TabPage3.Controls.Add(Me.GetResult)
+        Me.TabPage3.Controls.Add(Me.GroupBox3)
+        Me.TabPage3.Controls.Add(Me.GroupBox5)
+        Me.TabPage3.Controls.Add(Me.GroupBox2)
+        Me.TabPage3.Controls.Add(Me.LableResult)
+        Me.TabPage3.Controls.Add(Me.GroupBox4)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(712, 485)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "คำนวนผลแพ้ชนะ"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'ButtonReset
+        '
+        Me.ButtonReset.Location = New System.Drawing.Point(147, 297)
+        Me.ButtonReset.Name = "ButtonReset"
+        Me.ButtonReset.Size = New System.Drawing.Size(135, 58)
+        Me.ButtonReset.TabIndex = 5
+        Me.ButtonReset.Text = "Reset"
+        Me.ButtonReset.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(782, 450)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.ClientSize = New System.Drawing.Size(743, 533)
+        Me.Controls.Add(Me.TabControlMain)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureHero1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.PictureHero2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        CType(Me.PictureHero3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox4.ResumeLayout(False)
+        CType(Me.PictureHero4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox5.ResumeLayout(False)
+        CType(Me.PictureHero5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControlMain.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        CType(Me.GraphDisplay, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage3.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureHero1 As PictureBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents PictureHero2 As PictureBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents PictureHero3 As PictureBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents PictureHero4 As PictureBox
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents PictureHero5 As PictureBox
+    Friend WithEvents LableResult As Label
+    Friend WithEvents GetResult As Button
+    Friend WithEvents TabControlMain As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents GraphLabel As Label
+    Friend WithEvents GraphSelecter As ComboBox
+    Friend WithEvents GraphDisplay As PictureBox
+    Friend WithEvents InformationLabel As Label
+    Friend WithEvents HeroSelect1 As ComboBox
+    Friend WithEvents HeroSelect2 As ComboBox
+    Friend WithEvents HeroSelect3 As ComboBox
+    Friend WithEvents HeroSelect4 As ComboBox
+    Friend WithEvents HeroSelect5 As ComboBox
+    Friend WithEvents ButtonReset As Button
 End Class
--- VBDota2WinPredic/VBDota2WinPredic/Form1.vb
+++ VBDota2WinPredic/VBDota2WinPredic/Form1.vb
@@ -1,3 +1,38 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Reset()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonReset.Click
+        Reset()
+    End Sub
+
+    Private Sub Reset()
+
+    End Sub
+    Private Sub HeroSelect1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles HeroSelect1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub HeroSelect2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles HeroSelect2.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub HeroSelect3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles HeroSelect3.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub HeroSelect4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles HeroSelect4.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub HeroSelect5_SelectedIndexChanged(sender As Object, e As EventArgs) Handles HeroSelect5.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ClearDuplicationHero(sender As Object, e As EventArgs) Handles HeroSelect5.SelectedIndexChanged, HeroSelect4.SelectedIndexChanged, HeroSelect3.SelectedIndexChanged, HeroSelect2.SelectedIndexChanged, HeroSelect1.SelectedIndexChanged
+
+    End Sub
+
 
 End Class
